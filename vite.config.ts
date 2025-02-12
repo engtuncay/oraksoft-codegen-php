2222b713f18624d0c9f61139fceff402b3f244aa
TuncayHp <tunc270@gmail.com>
2025-02-13 01:59:53 +0300

    first

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: 'index.html',  // Giriş noktası olarak TypeScript dosyasını belirt
+            output: {
+                entryFileNames: 'main.js',        // Hash olmadan dosya adı
+                chunkFileNames: 'chunk-[name].js',  // Diğer parçalar için isimlendirme
+                assetFileNames: '[name].[ext]'    // CSS veya diğer varlıklar için isimlendirme
+            }
+        },
+        outDir: 'dist/assets',
+        emptyOutDir: true
+    }
+});
