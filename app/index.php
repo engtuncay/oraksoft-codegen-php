d6faee01a9c97ee50a010036c1e9bf8c04a1159f
TuncayHp <tunc270@gmail.com>
2025-02-16 02:00:54 +0300

    up

--- app/index.php
+++ app/index.php
@@ -8,8 +8,10 @@ require 'fiAppImports.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orak Soft Code Generator</title>
     <link rel="stylesheet" href="libs/bootstrap.min.css">
-    <script type="module" crossorigin src="./assets/main.js"></script>
+    <!--    <script type="module" crossorigin src="./assets/main.js"></script>-->
+    <!--    <script defer src="libs/cdn.min.js"></script>-->
     <!--<script src="http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.5.0.js"></script>-->
+    <script src="libs/knockout-latest.js"></script>
     <!--<script src="fiapp.js"></script>-->
     <style>
         .fibody {
@@ -29,12 +31,13 @@ require 'fiAppImports.php'
 
                 <div class="card-body">
 
-                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <form @submit.prevent="validateForm" action="upload.php" method="post"
+                          enctype="multipart/form-data">
 
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-4 mb-3">
-                                    <label for="selectFi" class="form-label">Typescript:</label>
+                                    <label for="selectFi" class="form-label">Csharp:</label>
                                     <select class="form-select" aria-label="Default select example" name="selectFi"
                                             id="selectFi">
                                         <option value="-1" selected>Seçiniz</option>
@@ -65,6 +68,7 @@ require 'fiAppImports.php'
                         </div>
                         <div class="d-grid">
                             <button type="submit" class="btn btn-primary">Yükle</button>
+
                         </div>
                     </form>
                 </div>
@@ -73,12 +77,38 @@ require 'fiAppImports.php'
     </div>
 </div>
 
+<!--<h2>Renk Seçin</h2>-->
+<!---->
+<!-- Select elementi -->
+<!--<select data-bind="options: colors, value: selectedColor, optionsText: 'name', optionsValue: 'code'">-->
+<!--</select>-->
+<!---->
+<!-- Seçilen rengi göster -->
+<!--<p>Seçilen Renk: <span data-bind="text: selectedColor"></span></p>-->
+
+
 <script>
+    function AppViewModel() {
+        var self = this;
 
+        // Renk seçenekleri
+        self.colors = [
+            {name: "Kırmızı", code: "#FF0000"},
+            {name: "Yeşil", code: "#00FF00"},
+            {name: "Mavi", code: "#0000FF"},
+            {name: "Sarı", code: "#FFFF00"}
+        ];
+
+        // Seçilen renk
+        self.selectedColor = ko.observable(self.colors[0].code); // Varsayılan olarak ilk renk seçili
+
+        // Seçilen renk değeri değiştikçe burada bir şeyler yapılabilir.
+    }
+
+    ko.applyBindings(new AppViewModel());
 </script>
-
-<!--<script type="module" src="./assets/main.js"></script>-->
 <script src="libs/bootstrap.min.js"></script>
 <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>-->
+<!--<script type="module" src="./assets/main.js"></script>-->
 </body>
 </html>
---
+++ readme.md
@@ -0,0 +1,21 @@
+
+# copyDeps script'i yapılarak, kütüphaneler app/libs içerisine kopyalanması sağlanmıştır. php uygulaması app içerisine konumlandırılmıştır.
+
+# Bazı paketler
+
+- "knockout": "^3.5.1"
+
+  {
+  "name": "knockout",
+  "file": "build/output/knockout-latest.js"
+  }
+
+- alpine
+
+  {
+  "name": "alpine",
+  "file": "dist/cdn.min.js"
+  }
+
+
+
