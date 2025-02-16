b5375571759135aa7d413313b086274b4ea5f4b6
TuncayHp <tunc270@gmail.com>
2025-02-16 05:52:02 +0300

    up

--- app/main.mjs
+++ app/main.mjs
@@ -1,27 +1,20 @@
+//import { FiKeybean } from "./libs/oraksoft-ui.js";
+
 let elementById = document.getElementById("opsCsharp");
 
-const option0 = document.createElement("option");
-option0.value = "-1";
-option0.textContent = "Seçiniz";
+function addOption(element, value,textContent) {
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = textContent;
+    element.appendChild(option);
+}
 
-const option1 = document.createElement("option");
-option1.value = "1";
-option1.textContent = "FiCol";
+addOption(elementById,"-1","Seçiniz");
+addOption(elementById,"1","FiCol");
+addOption(elementById,"2","opt2");
 
-const option2 = document.createElement("option");
-option2.value = "2";
-option2.textContent = "opt2";
-
-elementById.appendChild(option0);
-elementById.appendChild(option1);
-elementById.appendChild(option2);
+//let fkb = new Fikeybean();
 
 //import { Fdr } from './libs/oraksoft-ui.js';
 //console.log(greet("dünya"));
 //let fiCol = new FiCol();
-
-
-
-
-
-
