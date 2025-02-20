1be1e895b679227079f385559f94f0d585d481d3
Tuncay <tunc270@gmail.com>
2025-02-20 13:39:39 +0300

    up

--- app/codegen.php
+++ app/codegen.php
@@ -1,144 +1,161 @@
 <?php
 require 'fiAppImports.php';
 
+use Engtuncay\Phputils8\meta\Fdr;
 use Engtuncay\Phputils8\meta\FiCol;
 use Engtuncay\Phputils8\meta\FiColList;
-use PhpOffice\PhpSpreadsheet\IOFactory;
+
+//use PhpOffice\PhpSpreadsheet\IOFactory;
 use Engtuncay\Phputils8\log\FiLog;
 
 FiLog::initLogger('filog');
 
+$fdrExcel = new Fdr();
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['excelFile'])) {
-  $uploadedFile = $_FILES['excelFile'];
+    $uploadedFile = $_FILES['excelFile'];
+    // Dosya geçici olarak kaydediliyor
+    $_SESSION["uploaded_file"] = $_FILES["excelFile"]["name"];
 
-  if ($uploadedFile['error'] !== UPLOAD_ERR_OK) {
-    die('Dosya yüklenirken hata oluştu.');
-  }
+    if ($uploadedFile['error'] !== UPLOAD_ERR_OK) {
+        //die('Dosya yüklenirken hata oluştu.');
+        $fdrExcel->setMessage("Dosya yüklenirken Hata oluştu.");
+        goto endExcelOkuma;
+    }
 
-  $fileExtension = pathinfo($uploadedFile['name'], PATHINFO_EXTENSION);
-  $allowedExtensions = ['xlsx', 'xls', 'csv'];
+    $fileExtension = pathinfo($uploadedFile['name'], PATHINFO_EXTENSION);
+    $allowedExtensions = ['xlsx', 'xls', 'csv'];
 
-  if (!in_array(strtolower($fileExtension), $allowedExtensions)) {
-    die('Geçersiz dosya formatı. Sadece .xlsx, .xls veya .csv dosyaları yükleyebilirsiniz.');
-  }
+    if (!in_array(strtolower($fileExtension), $allowedExtensions)) {
+        $mess = 'Geçersiz dosya formatı. Sadece .xlsx, .xls veya .csv dosyaları yükleyebilirsiniz.';
+        //die($mess);
+        $fdrExcel->setMessage($mess);
+        goto endExcelOkuma;
+    }
 
-  $inputFileName = $uploadedFile['tmp_name'];
+    $inputFileName = $uploadedFile['tmp_name'];
 
-  $fiExcel = new \Engtuncay\Phputils8\excel\FiExcel();
+    $fiExcel = new \Engtuncay\Phputils8\excel\FiExcel();
 
-  $fiCols = new FiColList();
-  $fiCol = new FiCol();
-  $fiCol->ofcTxFieldName = "A1";
-  $fiCol->ofcTxHeader = "A1";
+    $fiCols = new FiColList();
+    $fiCol = new FiCol();
+    $fiCol->ofcTxFieldName = "A1";
+    $fiCol->ofcTxHeader = "A1";
 
-  $fiCols->add($fiCol);
+    $fiCols->add($fiCol);
 
-  $fdrExcel = $fiExcel::readExcelFile($inputFileName, $fiCols);
+    $fdrExcel = $fiExcel::readExcelFile($inputFileName, $fiCols);
 
-  //print_r($fdr);
-  //echo var_export($fdr->getFkbList(), true);
-  //echo PHP_EOL;
-  //print_r($fdr->getFkbList());
+    //print_r($fdr);
+    //echo var_export($fdr->getFkbList(), true);
+    //echo PHP_EOL;
+    //print_r($fdr->getFkbList());
 
-  // Formdan gelen POST verilerini al
-  $formData = $_POST;
+    // Formdan gelen POST verilerini al
+    $formData = $_POST;
 
-  // stdClass nesnesine dönüştür
-  $formObject = (object)$formData;
+    // stdClass nesnesine dönüştür
+    $formObject = (object)$formData;
 
-  // Nesne olarak verileri görüntüle
-//  echo "Ad: " . $formObject->name . "\n";
-//  echo("<br/>");
-//  echo "Email: " . $formObject->email . "\n";
-//  echo("<br/>");
-  //print_r($formObject);
+    // Nesne olarak verileri görüntüle
+    //  echo "Ad: " . $formObject->name . "\n";
+    //  echo("<br/>");
+    //  echo "Email: " . $formObject->email . "\n";
+    //  echo("<br/>");
+    //print_r($formObject);
 } else {
-  $fdrExcel = new \Engtuncay\Phputils8\meta\Fdr(false, "No Excel File");
+    $fdrExcel = new Fdr(false, "No Excel Upload File");
 }
-?>
-    <!doctype html>
-    <html lang="tr">
-    <head>
-      <?php require 'fiHead.php'; ?>
-        <link rel="stylesheet" href="codeblock.css">
-    </head>
-    <body>
-    <h1>Codegen</h1>
 
+endExcelOkuma:
+
+
+?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <?php require 'fiHead.php'; ?>
+    <link rel="stylesheet" href="codeblock.css">
+</head>
+<body class="fibody">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">-->
+<div class="container mt-3">
+    <!--code blok -->
     <div class="position-relative">
     <pre class="p-3 rounded"><code id="code-snippet">
 const hello = "Hello, Bootstrap!";
 console.log(hello);
     </code></pre>
-    <button class="btn btn-sm btn-outline-light position-absolute top-0 end-0 m-2 copy-btn" onclick="copyCode()">
-    Copy
-    </button>
+        <button class="btn btn-sm btn-outline-light position-absolute top-0 end-0 m-2 copy-btn" onclick="copyCode()">
+            Copy
+        </button>
     </div>
 
-    <div>
-      <?php //print_r($fdrExcel?->getFkbListInit()?->getAsMultiArray()) ?>
-    </div>
-
-    <script>
-
-        function copyCode() {
-            const code = document.getElementById("code-snippet").innerText;
-            navigator.clipboard.writeText(code).then(() => {
-                alert("Copied!");
-            });
-        }
-
-        // Butonun tıklanmasıyla fonksiyonu çalıştır
-        //document.getElementById("copy-btn").addEventListener("click", copyCode);
-
-        var fkb =<?php echo json_encode($fdrExcel->getFkbListInit()->getAsMultiArray()); ?>;
-        //document.getElementById("#txaOutput").textContent = fkb.toString();
-        for (const fkbElement of fkb) {
-            console.log(fkbElement);
-            document.getElementById("#txaOutput").textContent = fkbElement;
-        }
+</div>
 
 
-        console.log(fkb);
+<!--<h2>Renk Seçin</h2>-->
+<!---->
+<!-- Select elementi -->
+<!--<select data-bind="options: colors, value: selectedColor, optionsText: 'name', optionsValue: 'code'">-->
+<!--</select>-->
+<!---->
+<!-- Seçilen rengi göster -->
+<!--<p>Seçilen Renk: <span data-bind="text: selectedColor"></span></p>-->
 
-    </script>
-    </body>
-    </html>
-<?php
-//    try {
-//        $spreadsheet = IOFactory::load($inputFileName);
-//        $sheet = $spreadsheet->getActiveSheet();
-//
-//        // En yüksek satır ve sütun numaralarını al
-//        $highestRow = $sheet->getHighestRow(); // Toplam satır sayısı
-//        $highestColumn = $sheet->getHighestColumn(); // En yüksek sütun harfi (örneğin, "D")
-//        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // Sütun harfini indekse çevir (örneğin, "D" -> 4)
-//
-//        // Satırları dolaş
-//        for ($row = 1; $row <= $highestRow; $row++) {
-//            // Sütunları dolaş
-//            $fkb = new \Engtuncay\Phputils\core\FiKeybean();
-//            for ($col = 1; $col <= $highestColumnIndex; $col++) {
-//                // Hücre değerini al
-//                $cellValue = $sheet->getCellByColumnAndRow($col, $row)->getValue();
-//                echo $cellValue . "\t";
-//            }
-//            echo PHP_EOL; // Satır sonu
-//        }
-//
-////        foreach ($sheet->getRowIterator() as $row) {
-////            $cellIterator = $row->getCellIterator();
-////            $cellIterator->setIterateOnlyExistingCells(false);
-////
-////            foreach ($cellIterator as $cell) {
-////                echo $cell->getValue() . "\t";
-////            }
-////            echo PHP_EOL;
-////        }
-//    } catch (Exception $e) {
-//        echo 'Excel dosyası okunurken hata oluştu: ', $e->getMessage(), PHP_EOL;
-//    }
-//} else {
-//    echo 'Lütfen bir dosya seçin.';
 
-//}
+<script>
+
+    function copyCode() {
+        const code = document.getElementById("code-snippet").innerText;
+        navigator.clipboard.writeText(code).then(() => {
+            //alert("Copied!");
+        });
+    }
+
+    // @flow
+    // let elementById1 = document.getElementById("#txaOutput");
+
+    // Butonun tıklanmasıyla fonksiyonu çalıştır
+    //document.getElementById("copy-btn").addEventListener("click", copyCode);
+
+    var fkb =<?php echo json_encode($fdrExcel->getFkbListInit()->getAsMultiArray()); ?>;
+    var fdrResult =<?php echo json_encode($fdrExcel->getBoResult())?>;
+
+    console.log(fkb);
+    console.log(fdrResult);
+
+    //document.getElementById("#txaOutput").textContent = fkb.toString();
+    // for (const fkbElement of fkb) {
+    //     console.log(fkbElement);
+    //     document.getElementById("#txaOutput").textContent = fkbElement;
+    // }
+    //console.log(fkb);
+
+    // function AppViewModel() {
+    //     var self = this;
+    //
+    //     // Renk seçenekleri
+    //     self.colors = [
+    //         {name: "Kırmızı", code: "#FF0000"},
+    //         {name: "Yeşil", code: "#00FF00"},
+    //         {name: "Mavi", code: "#0000FF"},
+    //         {name: "Sarı", code: "#FFFF00"}
+    //     ];
+    //
+    //     // Seçilen renk
+    //     self.selectedColor = ko.observable(self.colors[0].code); // Varsayılan olarak ilk renk seçili
+    //
+    //     // Seçilen renk değeri değiştikçe burada bir şeyler yapılabilir.
+    // }
+
+    // ko.applyBindings(new AppViewModel());
+</script>
+<!--page scripts-->
+<script src="libs/bootstrap.min.js"></script>
+<!--<script type="module" src="main.mjs"></script>-->
+<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>-->
+<!--<script type="module" src="./assets/main.js"></script>-->
+</body>
+</html>
--- app/index.php
+++ app/index.php
@@ -1,23 +1,30 @@
 <?php
-require 'fiAppImports.php'
+require 'fiAppImports.php';
+
+use Engtuncay\Phputils8\meta\Fdr;
+use Engtuncay\Phputils8\log\FiLog;
+
+FiLog::initLogger('filog');
+
 ?>
 <!DOCTYPE html>
 <html lang="tr">
 <head>
   <?php require 'fiHead.php'; ?>
+    <!--    <link rel="stylesheet" href="codeblock.css">-->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">-->
 </head>
 <body class="fibody">
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">-->
+<!---->
 <div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header bg-primary text-white">
                     <h3 class="card-title text-center">Orak Soft Code Generator</h3>
                 </div>
 
                 <div class="card-body">
-
                     <form action="codegen.php" method="post" enctype="multipart/form-data">
                         <div class="container">
                             <div class="row">
@@ -48,7 +55,7 @@ require 'fiAppImports.php'
                             <div class="form-text">Sadece .xlsx, .xls veya .csv dosyaları yükleyebilirsiniz.</div>
                         </div>
                         <div class="d-grid">
-                            <button type="submit" class="btn btn-primary">Yükle</button>
+                            <button type="submit" class="btn btn-primary">Generate Code</button>
 
                         </div>
                     </form>
@@ -69,26 +76,47 @@ require 'fiAppImports.php'
 
 
 <script>
-    function AppViewModel() {
-        var self = this;
 
-        // Renk seçenekleri
-        self.colors = [
-            {name: "Kırmızı", code: "#FF0000"},
-            {name: "Yeşil", code: "#00FF00"},
-            {name: "Mavi", code: "#0000FF"},
-            {name: "Sarı", code: "#FFFF00"}
-        ];
+    // function copyCode() {
+    //     const code = document.getElementById("code-snippet").innerText;
+    //     navigator.clipboard.writeText(code).then(() => {
+    //         //alert("Copied!");
+    //     });
+    // }
 
-        // Seçilen renk
-        self.selectedColor = ko.observable(self.colors[0].code); // Varsayılan olarak ilk renk seçili
+    // @flow
+    // let elementById1 = document.getElementById("#txaOutput");
 
-        // Seçilen renk değeri değiştikçe burada bir şeyler yapılabilir.
-    }
+    // Butonun tıklanmasıyla fonksiyonu çalıştır
+    //document.getElementById("copy-btn").addEventListener("click", copyCode);
 
-    ko.applyBindings(new AppViewModel());
+    //document.getElementById("#txaOutput").textContent = fkb.toString();
+    // for (const fkbElement of fkb) {
+    //     console.log(fkbElement);
+    //     document.getElementById("#txaOutput").textContent = fkbElement;
+    // }
+    //console.log(fkb);
+
+    // function AppViewModel() {
+    //     var self = this;
+    //
+    //     // Renk seçenekleri
+    //     self.colors = [
+    //         {name: "Kırmızı", code: "#FF0000"},
+    //         {name: "Yeşil", code: "#00FF00"},
+    //         {name: "Mavi", code: "#0000FF"},
+    //         {name: "Sarı", code: "#FFFF00"}
+    //     ];
+    //
+    //     // Seçilen renk
+    //     self.selectedColor = ko.observable(self.colors[0].code); // Varsayılan olarak ilk renk seçili
+    //
+    //     // Seçilen renk değeri değiştikçe burada bir şeyler yapılabilir.
+    // }
+
+    // ko.applyBindings(new AppViewModel());
 </script>
-<script type="module" src="main.mjs"></script>
+<script type="module" src="main.js"></script>
 <script src="libs/bootstrap.min.js"></script>
 <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>-->
 <!--<script type="module" src="./assets/main.js"></script>-->
