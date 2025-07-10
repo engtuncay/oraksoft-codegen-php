d5499929afcd836ed372b07030f161196bd9f4f0
TuncayHp2 <tunc270@gmail.com>
2025-07-11 00:19:27 +0300

    up

--- ocg/app/Views/fiHead.php
+++ ocg/app/Views/fiHead.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Orak Soft Code Generator</title>
-<link rel="stylesheet" href="libs/bootstrap.min.css">
+<link rel="stylesheet" href="<?=base_url('libs/bootstrap.min.css')?>">
 <!--<script type="module" crossorigin src="./assets/main.js"></script>-->
 <!--<script defer src="libs/cdn.min.js"></script>-->
 <!--<script src="http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.5.0.js"></script>-->
@@ -9,6 +9,6 @@
 <!--<script src="fiapp.js"></script>-->
 <style>
     .fibody {
-        background-image: url("./img/ocg-background.jpeg");
+        background-image: url("<?=base_url('assets/img/ocg-background.jpeg')?>");
     }
 </style>
---
+++ ocg/public/assets/js/main.js
@@ -0,0 +1,38 @@
+//import { FiKeybean } from "./libs/oraksoft-ui.js";
+
+let elementById = document.getElementById("selCsharp");
+
+function addOption(element, value,textContent) {
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = textContent;
+    element.appendChild(option);
+}
+
+let txIdSeciniz = "-1";
+
+addOption(elementById,txIdSeciniz,"Seçiniz");
+addOption(elementById,"1","FiCol Csharp Sınıf");
+
+let eleSelPhp = document.getElementById("selPhp");
+
+addOption(eleSelPhp,txIdSeciniz,"Seçiniz");
+addOption(eleSelPhp,"1","FiCol Php Sınıf");
+addOption(eleSelPhp,"2","FiMeta Php Sınıf");
+
+let eleSelTs = document.getElementById("selTs");
+
+addOption(eleSelTs,txIdSeciniz,"Seçiniz");
+addOption(eleSelTs,"1","FiCol Ts Sınıf");
+
+let eleSelJava = document.getElementById("selJava");
+
+addOption(eleSelJava,txIdSeciniz,"Seçiniz");
+addOption(eleSelJava,"1","FiCol Java Sınıf");
+
+
+//let fkb = new Fikeybean();
+
+//import { Fdr } from './libs/oraksoft-ui.js';
+//console.log(greet("dünya"));
+//let fiCol = new FiCol();
