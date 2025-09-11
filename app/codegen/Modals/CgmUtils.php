d8f1113d7c209ba28aebb140696d70382faaf462
Tuncay <tunc270@gmail.com>
2025-09-11 14:09:52 +0300

    up

--- app/codegen/Modals/CgmUtils.php
+++ app/codegen/Modals/CgmUtils.php
@@ -3,6 +3,7 @@
 namespace Codegen\Modals;
 
 use Codegen\ficols\FicFiCol;
+use Engtuncay\Phputils8\Core\FiString;
 use Engtuncay\Phputils8\Core\FiText;
 use Engtuncay\Phputils8\Core\FiwArray;
 use Engtuncay\Phputils8\Log\FiLog;
@@ -115,5 +116,50 @@ class CgmUtils
     //FiLog::$log?->debug(FiText::textArrayFkbList($fwarFkbEntity->getArrValue()));
 
     return $fwarFkbEntity->getArrValue();
+  }
+
+  /**
+   * Converts a field name to standard method name format.
+   *
+   * @param string|null $fieldName
+   * @return string
+   */
+  public static function convertFieldNameToLowerCamelCase(string $fieldName): string
+  {
+    // Başlangıçta eğer fieldName boşsa direkt döndür
+    if (FiString::isEmpty($fieldName)) return "";
+
+    if (!FiString::hasLowercaseLetter($fieldName)) {
+      $fieldName = strtolower($fieldName);
+      return lcfirst($fieldName);
+    } else {
+
+      $characters = str_split($fieldName); // Dizeyi karakterlere böl
+      $result = ''; // Sonuç dizesi oluştur
+      $length = count($characters);
+
+      for ($i = 0; $i < $length; $i++) {
+        // İlk harf her zaman küçük kalacak
+        if ($i === 0) {
+          $result .= strtolower($characters[$i]);
+          $characters[$i] = strtolower($characters[$i]);
+          continue;
+        }
+
+        // Kendinden önceki küçükse, aynen ekle
+        if (ctype_lower($characters[$i - 1])) { // && ctype_lower($characters[$i])
+          $result .= $characters[$i];
+        } // Kendinden önceki büyükse küçült
+        else if (ctype_upper($characters[$i - 1])) {
+          $result .= strtolower($characters[$i]);
+        } else if ($characters[$i - 1] == '_') {
+          $result .= strtolower($characters[$i]);
+        } else {  // Kendinden önceki sayı vs (_ dışında karakterse) ise büyült
+          $result .= strtoupper($characters[$i]);
+        }
+      }
+
+      return $result;
+    }
   }
 }
--- app/codegen/Modals/CogSpecsPhp.php
+++ app/codegen/Modals/CogSpecsPhp.php
@@ -37,52 +37,9 @@ class CogSpecsPhp implements ICogSpecs
    */
   public function checkMethodNameStd(mixed $fieldName): string
   {
-    return self::convertFieldNameToLowerCamelCase($fieldName);
+    return CgmUtils::convertFieldNameToLowerCamelCase($fieldName);
   }
 
-  /**
-   * Converts a field name to standard method name format.
-   *
-   * @param string|null $fieldName
-   * @return string
-   */
-  public static function convertFieldNameToLowerCamelCase(string $fieldName): string
-  {
-    // Başlangıçta eğer fieldName boşsa direkt döndür
-    if (FiString::isEmpty($fieldName)) return "";
 
-    if (!FiString::hasLowercaseLetter($fieldName)) {
-      $fieldName = strtolower($fieldName);
-      return lcfirst($fieldName);
-    } else {
-
-      $characters = str_split($fieldName); // Dizeyi karakterlere böl
-      $result = ''; // Sonuç dizesi oluştur
-      $length = count($characters);
-
-      for ($i = 0; $i < $length; $i++) {
-        // İlk harf her zaman küçük kalacak
-        if ($i === 0) {
-          $result .= strtolower($characters[$i]);
-          $characters[$i] = strtolower($characters[$i]);
-          continue;
-        }
-
-        // Kendinden önceki küçükse, aynen ekle
-        if (ctype_lower($characters[$i - 1])) { // && ctype_lower($characters[$i])
-          $result .= $characters[$i];
-        } // Kendinden önceki büyükse küçült
-        else if (ctype_upper($characters[$i - 1])) {
-          $result .= strtolower($characters[$i]);
-        } else if ($characters[$i - 1] == '_') {
-          $result .= strtolower($characters[$i]);
-        } else {  // Kendinden önceki sayı vs (_ dışında karakterse) ise büyült
-          $result .= strtoupper($characters[$i]);
-        }
-      }
-
-      return $result;
-    }
-  }
   
 }
